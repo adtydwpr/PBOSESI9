fc85bc86204e43b02dcdeb50b9f1290f65d14cff
adtydwpr <114389480+adtydwpr@users.noreply.github.com>
2022-12-23 17:24:43 +0700

    Create Karakter.java

---
+++ PBO2/deque/Karakter.java
@@ -0,0 +1,9 @@
+package PBOSESI9.deque;
+
+public abstract class Karakter {
+    abstract String getName();
+    abstract float getHp();
+    abstract float getAttack();
+    abstract float getDefense();
+    public abstract void infoKarakter();
+}
