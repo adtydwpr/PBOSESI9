dc816cbafe5dec6dc442dfb1b1c5c6ff531b9082
adtydwpr <114389480+adtydwpr@users.noreply.github.com>
2022-12-23 17:22:19 +0700

    Create MahasiswaKu.java

---
+++ PBO1/MahasiswaKu.java
@@ -0,0 +1,14 @@
+package PBOSESI9;
+
+public class MahasiswaKu {
+    public static void main(String[] args) {
+        Mahasiswa <String,String,Integer> m = new Mahasiswa<>();
+        m.setNim("20220050001");
+        m.setName("Sambo");
+        m.setClas(22);
+
+        System.out.println(m.getNim());
+        System.out.println(m.getName());
+        System.out.println(m.getClas());
+    }   
+}
